826cdc64f81f21022fe57b52b54bea5427870603
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-01-16 17:14:06 +0100

    chart-selection stylen+funktion
    Style und funktion der chart selection begonnen zu implementieren

---
+++ src/js/chart-selection.js
@@ -0,0 +1,12 @@
+function showLokalisation() {
+  alert('Die Daten bezüglich Lokalisation werden jetzt angezeigt');
+  //Verändere die Klassen, damit das Element acitve ist
+  //weiter mit: https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-with-javascript
+  
+}
+function showAlter() {
+  alert('Die Daten bezüglich Alter werden jetzt angezeigt');
+}
+function showKonsumart() {
+  alert('Die Daten bezüglich Konsumentenart werden jetzt angezeigt');
+}
