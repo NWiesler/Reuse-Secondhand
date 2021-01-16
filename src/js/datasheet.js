d6ae95ceb53edfb66cf269568a46269aa20d86f2
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-01-16 16:15:08 +0100

    Css Chart

--- src/js/datasheet.js
+++ src/js/datasheet.js
@@ -69,11 +69,12 @@ Highcharts.chart('container', {
 //Second Chart
 
 Highcharts.chart('container2', {
-    chart: {        
+    chart: { 
+        styledMode: true,       
         inverted: true
     },
     title: {
-        text: 'Meinung'
+        text: 'Verhalten'
     },
     accessibility: {
         keyboardNavigation: {
@@ -90,16 +91,15 @@ Highcharts.chart('container2', {
   
     },
     xAxis: {
+        opposite: true,
         categories: [
-            'Nicht nur beim Konsum, auch im Alltag wird nachhaltiges Handeln wichtiger.',
-            'Gebrauchte Produkte sind gut für die Umwelt.',
-            'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
-            'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung ',
-            'Ich achte im Alltag sehr darauf, Ressourcen zu schonen.',
-            'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
-            'Nachhaltig zu konsumieren bedeutet für mich weniger zu kaufen.',
-            'Nachhaltig zu konsumieren bedeutet für mich gebrauchte Produkte zu kaufen.',
-            'Gebrauchte Produkte sind etwas für Leute, die sich neue Ware nicht leisten können.'
+            'Ich versuche Produkte so lange wie möglich zu nutzen.',
+            'Jeder trägt eine eigene Verantwortung, sich nachhaltig zu verhalten.',
+            'Ich kann mir vorstellen, in Zukunft häufiger gebrauchte Produkte zu kaufen, um die Umwelt zu schonen.',
+            'Mich motiviert es, mich nachhaltig zu verhalten, wenn Personen in meinem dirketen Umfeld dies auch tun.',
+            'Ich finde die globale Schüler- und Studenten-Initiative "Fridays for Future" unterstützenswert.',
+            'Ich sehe nicht ein, für Umwelt und Klima Abstriche bei meinem Konsumverhalten zu machen.',
+            'Mir ist wichtig, immer die neusten Trends und Produkte zu besitzen.',
         ]
         
     },
@@ -125,13 +125,14 @@ Highcharts.chart('container2', {
         },
         area: {
             fillOpacity: 0.5
-        }
+        },
+    
     },
     series: [{
         name: 'Unter 30',
-        data: [69, 43, 72, 53, 48, 44, 44, 45, 21]
+        data: [79, 77, 50, 47, 45, 21, 18]
     }, {
         name: 'Über 30',
-        data: [72 , 65, 62, 61, 54, 48, 47, 40, 21]
+        data: [88 , 83, 46, 44, 38, 14, 10]
     }]
 });
