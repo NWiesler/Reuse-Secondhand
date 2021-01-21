3601ef4eb49a35e68f0e1129c7815a22cbe3f9e0
NWiesler <48588140+NWiesler@users.noreply.github.com>
2021-01-21 16:39:03 +0100

    Funktionaliät Chart Selection
    Funktionaliät Chart Selection

--- src/js/chart/chart-selection.js
+++ src/js/chart/chart-selection.js
@@ -1,12 +1,10 @@
 function showLokalisation() {
-  alert('Die Daten bezüglich Lokalisation werden jetzt angezeigt');
-  //Verändere die Klassen, damit das Element acitve ist
-  //weiter mit: https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-with-javascript 
   showLokalisationData()
+  setLokalisationActive()
 }
 function showAlter() {
-  alert('Die Daten bezüglich Alter werden jetzt angezeigt');
+  setAlterActive()
 }
 function showKonsumart() {
-  alert('Die Daten bezüglich Konsumentenart werden jetzt angezeigt');
+  setKonsumartActive()
 }
---
+++ src/js/chart/selection-change-style.js
@@ -0,0 +1,32 @@
+function setLokalisationActive(){
+    //Verändere die Klassen, damit das Element active ist
+    document.getElementById("icon-lokalisation").src="src/assets/charts/selected-localisation.svg";
+    document.getElementById("icon-alter").src="src/assets/charts/unselected-age.png";
+    document.getElementById("icon-konsumart").src="src/assets/charts/unselected-konsum.svg";
+
+    document.getElementById("btn-lokalisation").className = "btn-chart-selection active active-lokalisation";
+    document.getElementById("btn-alter").className = "btn-chart-selection disabled";
+    document.getElementById("btn-konsumart").className = "btn-chart-selection disabled";
+}
+
+function setAlterActive(){
+    //Verändere die Klassen, damit das Element active ist
+    document.getElementById("icon-lokalisation").src="src/assets/charts/unselected-localisation.svg";
+    document.getElementById("icon-alter").src="src/assets/charts/selected-age.png";
+    document.getElementById("icon-konsumart").src="src/assets/charts/unselected-konsum.svg";
+
+    document.getElementById("btn-lokalisation").className = "btn-chart-selection disabled";
+    document.getElementById("btn-alter").className = "btn-chart-selection active active-alter";
+    document.getElementById("btn-konsumart").className = "btn-chart-selection disabled";
+}
+
+function setKonsumartActive(){
+    //Verändere die Klassen, damit das Element active ist
+    document.getElementById("icon-lokalisation").src="src/assets/charts/unselected-localisation.svg";
+    document.getElementById("icon-alter").src="src/assets/charts/unselected-age.png";
+    document.getElementById("icon-konsumart").src="src/assets/charts/selected-konsum.svg";
+
+    document.getElementById("btn-lokalisation").className = "btn-chart-selection disabled";
+    document.getElementById("btn-alter").className = "btn-chart-selection disabled";
+    document.getElementById("btn-konsumart").className = "btn-chart-selection active active-konsumart";
+}
