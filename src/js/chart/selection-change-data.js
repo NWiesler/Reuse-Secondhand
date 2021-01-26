06ff780841d0ccbcb439e74b3a9fa36a3a1766c2
JuliaFischinger <48588116+JuliaFischinger@users.noreply.github.com>
2021-01-26 14:28:06 +0100

    Styling Chart

--- src/js/chart/selection-change-data.js
+++ src/js/chart/selection-change-data.js
@@ -3,6 +3,14 @@ document.getElementById('btn-alter').addEventListener('click', () => {
     chart1 = Highcharts.chart('container', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(238, 244, 242, 0.63)'],
+                    [1, 'rgba(228, 239, 235, 0.17)']
+    
+                ]
+            },
         },
         credits: {
             enabled: false
@@ -26,20 +34,24 @@ document.getElementById('btn-alter').addEventListener('click', () => {
             categories: [
                 'Nicht nur beim Konsum, auch im Alltag wird nachhaltiges Handeln wichtiger.',
                 'Gebrauchte Produkte sind gut für die Umwelt.',
-                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
-                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung ',
                 'Ich achte im Alltag sehr darauf, Ressourcen zu schonen.',
-                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
+                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung. ',
                 'Nachhaltig zu konsumieren bedeutet für mich weniger zu kaufen.',
+                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
+                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
                 'Nachhaltig zu konsumieren bedeutet für mich gebrauchte Produkte zu kaufen.',
                 'Gebrauchte Produkte sind etwas für Leute, die sich neue Ware nicht leisten können.'
             ],
-            crosshair: true,  
+            crosshair: {
+                width: 75,
+                color: 'rgba(110, 146, 163, 0.1)',
+                
+            },
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px',
                 }
-            }        
+            }               
         },
         yAxis: {
             minorTickInterval: 25,
@@ -67,6 +79,7 @@ document.getElementById('btn-alter').addEventListener('click', () => {
         series: [{
             name: 'Unter 30',
             color: '#5F8B96',
+            lineWidth: 3,
             data: [69, 43, 72, 53, 48, 44, 44, 45, 21],
             marker: {
                 fillColor: '#ffffff',
@@ -77,6 +90,7 @@ document.getElementById('btn-alter').addEventListener('click', () => {
         }, {
             name: 'Über 30',
             color: '#265870',
+            lineWidth: 3,
             data: [72, 65, 62, 61, 54, 48, 47, 40, 21],
             marker: {
                 symbol: 'circle',
@@ -92,6 +106,14 @@ document.getElementById('btn-alter').addEventListener('click', () => {
     chart2 = Highcharts.chart('container2', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(238, 244, 242, 0.63)'],
+                    [1, 'rgba(228, 239, 235, 0.17)']
+    
+                ]
+            },
         },
         credits: {
             enabled: false
@@ -114,20 +136,24 @@ document.getElementById('btn-alter').addEventListener('click', () => {
         xAxis: {
             opposite: true,
             categories: [
-                'Ich versuche Produkte so lange wie möglich zu nutzen.',
+                'Ich versuche, Produkte so lange wie möglich zu nutzen.',
                 'Jeder trägt eine eigene Verantwortung, sich nachhaltig zu verhalten.',
                 'Ich kann mir vorstellen, in Zukunft häufiger gebrauchte Produkte zu kaufen, um die Umwelt zu schonen.',
                 'Mich motiviert es, mich nachhaltig zu verhalten, wenn Personen in meinem dirketen Umfeld dies auch tun.',
                 'Ich finde die globale Schüler- und Studenten-Initiative "Fridays for Future" unterstützenswert.',
                 'Ich sehe nicht ein, für Umwelt und Klima Abstriche bei meinem Konsumverhalten zu machen.',
-                'Mir ist wichtig, immer die neusten Trends und Produkte zu besitzen.',
+                'Mir ist wichtig, immer die neuesten Trends und Produkte zu besitzen.',
             ],
-            crosshair: true,
+            crosshair: {
+                width: 75,
+                color: 'rgba(110, 146, 163, 0.1)',
+                
+            },
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px',
                 }
-            }          
+            }                 
         },
         yAxis: {
             minorTickInterval: 25,
@@ -155,6 +181,7 @@ document.getElementById('btn-alter').addEventListener('click', () => {
         series: [{
             name: 'Unter 30',
             color: '#5F8B96',
+            lineWidth: 3,
             data: [79, 77, 50, 47, 45, 21, 18],
             marker: {
                 fillColor: '#ffffff',
@@ -165,6 +192,7 @@ document.getElementById('btn-alter').addEventListener('click', () => {
         }, {
             name: 'Über 30',
             color: '#265870',
+            lineWidth: 3,
             data: [88 , 83, 46, 44, 38, 14, 10],
             marker: {
                 symbol: 'circle',
@@ -182,6 +210,14 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
     chart1 = Highcharts.chart('container', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(253, 253, 246, 1)'],
+                    [1, 'rgba(253, 249, 246, 0)']
+    
+                ]
+            },
         },
         credits: {
             enabled: false
@@ -205,20 +241,24 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
             categories: [
                 'Nicht nur beim Konsum, auch im Alltag wird nachhaltiges Handeln wichtiger.',
                 'Gebrauchte Produkte sind gut für die Umwelt.',
-                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
-                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung ',
                 'Ich achte im Alltag sehr darauf, Ressourcen zu schonen.',
-                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
+                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung. ',
                 'Nachhaltig zu konsumieren bedeutet für mich weniger zu kaufen.',
+                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
+                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
                 'Nachhaltig zu konsumieren bedeutet für mich gebrauchte Produkte zu kaufen.',
                 'Gebrauchte Produkte sind etwas für Leute, die sich neue Ware nicht leisten können.'
             ],
-            crosshair: true,  
+            crosshair: {
+                width: 75,
+                color: 'rgba(182, 118, 100, 0.1)',
+                
+            },
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px',
                 }
-            }        
+            }            
         },
         yAxis: {
             minorTickInterval: 25,
@@ -246,6 +286,7 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
         series: [{
             name: 'West',
             color: '#DEB89A',
+            lineWidth: 3,
             data: [72, 65, 63, 60, 54, 49, 46, 42, 20],
             marker: {
                 fillColor: '#ffffff',
@@ -256,6 +297,7 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
         }, {
             name: 'Ost',
             color: '#8B4E3E',
+            lineWidth: 3,
             data: [69, 60, 53, 56, 49, 42, 47, 38, 25],
             marker: {
                 symbol: 'circle',
@@ -271,6 +313,14 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
     chart2 = Highcharts.chart('container2', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(253, 253, 246, 1)'],
+                    [1, 'rgba(253, 249, 246, 0)']
+    
+                ]
+            },
         },
         credits: {
             enabled: false
@@ -293,20 +343,24 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
         xAxis: {
             opposite: true,
             categories: [
-                'Ich versuche Produkte so lange wie möglich zu nutzen.',
+                'Ich versuche, Produkte so lange wie möglich zu nutzen.',
                 'Jeder trägt eine eigene Verantwortung, sich nachhaltig zu verhalten.',
                 'Ich kann mir vorstellen, in Zukunft häufiger gebrauchte Produkte zu kaufen, um die Umwelt zu schonen.',
                 'Mich motiviert es, mich nachhaltig zu verhalten, wenn Personen in meinem dirketen Umfeld dies auch tun.',
                 'Ich finde die globale Schüler- und Studenten-Initiative "Fridays for Future" unterstützenswert.',
                 'Ich sehe nicht ein, für Umwelt und Klima Abstriche bei meinem Konsumverhalten zu machen.',
-                'Mir ist wichtig, immer die neusten Trends und Produkte zu besitzen.',
+                'Mir ist wichtig, immer die neuesten Trends und Produkte zu besitzen.',
             ],
-            crosshair: true,    
+            crosshair: {
+                width: 75,
+                color: 'rgba(182, 118, 100, 0.1)',
+                
+            },
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px',
                 }
-            }      
+            }         
         },
         yAxis: {
             minorTickInterval: 25,
@@ -334,7 +388,8 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
         series: [{
             name: 'West',
             color: '#DEB89A',
-            data: [87, 84, 47, 46, 40, 14, 11],
+            lineWidth: 3,
+            data: [87, 84, 47, 45, 40, 14, 11],
             marker: {
                 fillColor: '#ffffff',
                 lineColor: null,
@@ -344,6 +399,7 @@ document.getElementById('btn-lokalisation').addEventListener('click', () => {
         }, {
             name: 'Ost',
             color: '#8B4E3E',
+            lineWidth: 3,
             data: [86 , 77, 44, 44, 36, 20, 13],
             marker: {
                 symbol: 'circle',
@@ -361,6 +417,15 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
     chart1 = Highcharts.chart('container', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(243, 252, 250, 1)'],
+                    [1, 'rgba(243, 252, 250, 0)']
+    
+                ]
+            },
+            
         },
         credits: {
             enabled: false
@@ -384,18 +449,21 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
             categories: [
                 'Nicht nur beim Konsum, auch im Alltag wird nachhaltiges Handeln wichtiger.',
                 'Gebrauchte Produkte sind gut für die Umwelt.',
-                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
-                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung ',
                 'Ich achte im Alltag sehr darauf, Ressourcen zu schonen.',
-                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
+                'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung. ',
                 'Nachhaltig zu konsumieren bedeutet für mich weniger zu kaufen.',
+                'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
+                'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
                 'Nachhaltig zu konsumieren bedeutet für mich gebrauchte Produkte zu kaufen.',
                 'Gebrauchte Produkte sind etwas für Leute, die sich neue Ware nicht leisten können.'
             ],
-            crosshair: true,   
+            crosshair: {
+                width: 75,
+                color: 'rgba(132, 163, 148, 0.1)', 
+            }, 
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px'
                 }
             }       
         },
@@ -425,6 +493,7 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
         series: [{
             name: 'Neu-Käufer:innen',
             color: '#AFB878',
+            lineWidth: 3,
             data: [67, 55, 28, 53, 58, 35, 50, 25, 27],
             marker: {
                 fillColor: '#ffffff',
@@ -435,6 +504,7 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
         }, {
             name: 'Gebraucht-Käufer:innen',
             color: '#4A6E63',
+            lineWidth: 3,
             data: [75, 73, 66, 66, 64, 57, 56, 55, 16],
             marker: {
                 symbol: 'circle',
@@ -450,6 +520,14 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
     chart2 = Highcharts.chart('container2', {
         chart: {           
             inverted: true,
+            plotBackgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgba(243, 252, 250, 1)'],
+                    [1, 'rgba(243, 252, 250, 0)']
+    
+                ]
+            },
         },
         credits: {
             enabled: false
@@ -472,18 +550,21 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
         xAxis: {
             opposite: true,
             categories: [
-                'Ich versuche Produkte so lange wie möglich zu nutzen.',
+                'Ich versuche, Produkte so lange wie möglich zu nutzen.',
                 'Jeder trägt eine eigene Verantwortung, sich nachhaltig zu verhalten.',
                 'Ich kann mir vorstellen, in Zukunft häufiger gebrauchte Produkte zu kaufen, um die Umwelt zu schonen.',
                 'Mich motiviert es, mich nachhaltig zu verhalten, wenn Personen in meinem dirketen Umfeld dies auch tun.',
                 'Ich finde die globale Schüler- und Studenten-Initiative "Fridays for Future" unterstützenswert.',
                 'Ich sehe nicht ein, für Umwelt und Klima Abstriche bei meinem Konsumverhalten zu machen.',
-                'Mir ist wichtig, immer die neusten Trends und Produkte zu besitzen.',
+                'Mir ist wichtig, immer die neuesten Trends und Produkte zu besitzen.',
             ],
-            crosshair: true,  
+            crosshair: {
+                width: 75,
+                color: 'rgba(132, 163, 148, 0.1)', 
+            }, 
             labels:{
                 style:{
-                    fontSize: '14px'
+                    fontSize: '16px'
                 }
             }        
         },
@@ -513,6 +594,7 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
         series: [{
             name: 'Neu-Käufer:innen',
             color: '#AFB878',
+            lineWidth: 3,
             data: [86, 83, 30, 42, 33, 16, 12],
             marker: {
                 fillColor: '#ffffff',
@@ -523,6 +605,7 @@ document.getElementById('btn-konsumart').addEventListener('click', () => {
         }, {
             name: 'Gebraucht-Käufer:innen',
             color: '#4A6E63',
+            lineWidth: 3,
             data: [88 , 83, 63, 48, 45, 14, 10],
             marker: {
                 symbol: 'circle',
--- src/js/datasheet.js
+++ src/js/datasheet.js
@@ -1,6 +1,6 @@
 Highcharts.setOptions({
     chart: {
-        backgroundColor: '#ff0000',
+        backgroundColor: 'transparent',
         style: {
             fontFamily: 'Jost',
             fontSize: '20px',
@@ -10,6 +10,14 @@ Highcharts.setOptions({
 const chart1 = Highcharts.chart('container', {
     chart: {      
         inverted: true,
+        plotBackgroundColor: {
+            linearGradient: [0, 0, 500, 500],
+            stops: [
+                [0, 'rgba(238, 244, 242, 0.63)'],
+                [1, 'rgba(228, 239, 235, 0.17)']
+
+            ]
+        },
     },
     credits: {
         enabled: false
@@ -33,25 +41,28 @@ const chart1 = Highcharts.chart('container', {
         categories: [
             'Nicht nur beim Konsum, auch im Alltag wird nachhaltiges Handeln wichtiger.',
             'Gebrauchte Produkte sind gut für die Umwelt.',
-            'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
-            'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung ',
             'Ich achte im Alltag sehr darauf, Ressourcen zu schonen.',
-            'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
+            'Nachhaltiges Handeln im Alltag gewinnt für mich an Bedeutung. ',
             'Nachhaltig zu konsumieren bedeutet für mich weniger zu kaufen.',
+            'Gebrauchte Produkte sind für mich eine attraktive Alternative zu neuen Produkten.',
+            'Gebraucht Kaufen wird in Zukunft eine wirtschaftlich bessere Alternative zum Neukauf sein.',
             'Nachhaltig zu konsumieren bedeutet für mich gebrauchte Produkte zu kaufen.',
             'Gebrauchte Produkte sind etwas für Leute, die sich neue Ware nicht leisten können.'
         ],
-        crosshair: true, 
+          crosshair: {
+            width: 75,
+            color: 'rgba(110, 146, 163, 0.1)',
+            
+        },
         labels:{
             style:{
-                fontSize: '16px'
+                fontSize: '16px',
             }
         }        
     },
     yAxis: {
         minorTickInterval: 25,
         tickInterval: 100,
-        
         title: {
             text: 'Zustimmung'
         },
@@ -75,6 +86,7 @@ const chart1 = Highcharts.chart('container', {
     series: [{
         name: 'Unter 30',
         color: '#5F8B96',
+        lineWidth: 3,
         data: [69, 43, 72, 53, 48, 44, 44, 45, 21],
         marker: {
             fillColor: '#ffffff',
@@ -85,7 +97,7 @@ const chart1 = Highcharts.chart('container', {
     }, {
         name: 'Über 30',
         color: '#265870',
-        lineWidth: 5,
+        lineWidth: 3,
         data: [72, 65, 62, 61, 54, 48, 47, 40, 21],
         marker: {
             symbol: 'circle',
@@ -100,6 +112,13 @@ const chart1 = Highcharts.chart('container', {
 const chart2 = Highcharts.chart('container2', {
     chart: {           
         inverted: true,
+        plotBackgroundColor: {
+            linearGradient: [0, 0, 500, 500],
+            stops: [
+                [0, 'rgba(238, 244, 242, 0.63)'],
+                [1, 'rgba(228, 239, 235, 0.17)']
+            ]
+        },
     },
     credits: {
         enabled: false
@@ -122,20 +141,24 @@ const chart2 = Highcharts.chart('container2', {
     xAxis: {
         opposite: true,
         categories: [
-            'Ich versuche Produkte so lange wie möglich zu nutzen.',
+            'Ich versuche, Produkte so lange wie möglich zu nutzen.',
             'Jeder trägt eine eigene Verantwortung, sich nachhaltig zu verhalten.',
             'Ich kann mir vorstellen, in Zukunft häufiger gebrauchte Produkte zu kaufen, um die Umwelt zu schonen.',
             'Mich motiviert es, mich nachhaltig zu verhalten, wenn Personen in meinem dirketen Umfeld dies auch tun.',
             'Ich finde die globale Schüler- und Studenten-Initiative "Fridays for Future" unterstützenswert.',
             'Ich sehe nicht ein, für Umwelt und Klima Abstriche bei meinem Konsumverhalten zu machen.',
-            'Mir ist wichtig, immer die neusten Trends und Produkte zu besitzen.',
+            'Mir ist wichtig, immer die neuesten Trends und Produkte zu besitzen.',
         ],
-        crosshair: true,   
+        crosshair: {
+            width: 75,
+            color: 'rgba(110, 146, 163, 0.1)',
+            
+        },
         labels:{
             style:{
-                fontSize: '16px'
+                fontSize: '16px',
             }
-        }       
+        }           
     },
     yAxis: {
         minorTickInterval: 25,
@@ -163,6 +186,7 @@ const chart2 = Highcharts.chart('container2', {
     series: [{
         name: 'Unter 30',
         color: '#5F8B96',
+        lineWidth: 3,
         data: [79, 77, 50, 47, 45, 21, 18],
         marker: {
             fillColor: '#ffffff',
@@ -173,6 +197,7 @@ const chart2 = Highcharts.chart('container2', {
     }, {
         name: 'Über 30',
         color: '#265870',
+        lineWidth: 3,
         data: [88 , 83, 46, 44, 38, 14, 10],
         marker: {
             symbol: 'circle',
